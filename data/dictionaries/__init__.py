e7cf7c9b34d3ccffce56cba4bc36e5279926803b
Vladimir Podlevskikh <145929911+namebogsecret@users.noreply.github.com>
2025-05-19 23:56:43 +0400

    Add basic expressions feature

--- data/dictionaries/__init__.py
+++ data/dictionaries/__init__.py
@@ -6,10 +6,13 @@
 
 from data.dictionaries.armenian_dict import TRANSLATION_DICT, get_armenian_translation
 from data.dictionaries.transliteration import TRANSLITERATION_MAP, transliterate_text
+from data.dictionaries.expressions import EXPRESSIONS, get_random_expression
 
 __all__ = [
     'TRANSLATION_DICT',
     'get_armenian_translation',
     'TRANSLITERATION_MAP',
-    'transliterate_text'
+    'transliterate_text',
+    'EXPRESSIONS',
+    'get_random_expression',
 ]
---
+++ features/basic_expressions/__init__.py
@@ -0,0 +1,5 @@
+"""Utilities for learning basic words and expressions."""
+
+from .utils import get_random_word, get_random_expression
+
+__all__ = ["get_random_word", "get_random_expression"]
